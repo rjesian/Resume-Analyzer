60760b00d7ddf9f4fa74ac9561d70677d9712d5f
Rifah Jesian <rifahj808@gmail.com>
2026-02-06 23:33:11 -0500

    Added text cleaning function to remove punctuation, capitalization, and return words from files as list of strings

--- analyzer.py
+++ analyzer.py
@@ -1,3 +1,4 @@
+import re
 print("Resume Analyzer v0.01")
 
 def read_file(path):
@@ -9,5 +10,20 @@ job_description_text = read_file("data/job_description.txt")
 
 print("\n Resume Loaded:", len(resume_text), "characters")
 print("\n Job Description:", len(job_description_text), "characters")
+
+
+def clean_text(text):
+    """
+    Converts text to lowercase, removes punctuation, and splits into a list of words.
+    """
+    text = text.lower()
+    text = re.sub(r"[^a-z\s]", "", text)
+    return text.split()
+
+resume_words = clean_text(resume_text)
+job_words = clean_text(job_description_text)
+
+print("\n Resume Words:", resume_words[:10])
+print("\n Job Words:", job_words[:10])
 
 
